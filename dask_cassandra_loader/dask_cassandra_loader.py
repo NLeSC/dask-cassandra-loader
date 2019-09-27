dc444aa10b82e8036218b942f4a101bdc0bdf22b
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2019-09-27 15:36:15 +0200

    Remove white spaces and empty lines.

--- dask_cassandra_loader/dask_cassandra_loader.py
+++ dask_cassandra_loader/dask_cassandra_loader.py
@@ -10,10 +10,9 @@ class DaskCassandraLoader(object):
     """  A loader to populate a Dask Dataframe with data from a Cassandra table. """
 
     def __init__(self):
-        """ 
+        """
         Initialization of DaskCassandraLoader
         > DaskCassandraLoader()
-        
         """
         self.logger = logging.getLogger(__name__)
         self.error = None
@@ -27,8 +26,7 @@ class DaskCassandraLoader(object):
         """
         Connects to a local Dask cluster.
         > connect_to_local_dask()
-        
-        :return: 
+        :return:
         """
         self.logger.info('Create and connect to a local Dask cluster.')
         cluster = LocalCluster()  # kwargs={'local-directory':'/home/jupyter/'})
@@ -39,8 +37,7 @@ class DaskCassandraLoader(object):
         """
         Ends the established Dask connection.
         > disconnect_from_dask()
-        
-        :return: 
+        :return:
         """
         self.dask_client.close()
         return
@@ -49,10 +46,9 @@ class DaskCassandraLoader(object):
         """
         Connects to a Cassandra cluster specified by a list of IPs.
         > connect_to_cassandra('test', ['10.0.1.1', '10.0.1.2'])
-        
         :param cassandra_keyspace: It is a string which contains an existent Cassandra keyspace.
         :param cassandra_clusters: It is a list of IPs with each IP represented as a string.
-        :return: 
+        :return:
         """
         if cassandra_keyspace == "":
             raise Exception("Key space can't be an empty string!!!")
@@ -66,8 +62,7 @@ class DaskCassandraLoader(object):
         """
         Ends the established Cassandra connection.
         > disconnect_from_cassandra()
-        
-        :return: 
+        :return:
         """
         if self.cassandra_con is not None:
             self.cassandra_con.shutdown()
@@ -76,19 +71,18 @@ class DaskCassandraLoader(object):
     def load_cassandra_table(self, table_name, projections, and_predicates, partitions_to_load):
         """
         It loads a Cassandra table into a Dask dataframe.
-        > load_cassandra_table('tab1', 
-                ['id', 'year', 'month', 'day'], 
+        > load_cassandra_table('tab1',
+                ['id', 'year', 'month', 'day'],
                 [('month', 'less_than', 1), ('day', 'in_', [1,2,3,8,12,30])],
                  [(id, [1, 2, 3, 4, 5, 6]), ('year',[2019])] )
-                 
-        :param table_name: It is a String. 
+        :param table_name: It is a String.
         :param projections: A list of columns names. Each column name is a String.
         :param and_predicates: List of triples. Each triple contains column name as String,
         operator name as String, and a list of values depending on the operator. CassandraOperators.print_operators()
         prints all available operators. It should only contain columns which are not partition columns.
         :param partitions_to_load: List of tuples. Each tuple as a column name as String
         and a list of keys which should be selected. It should only contain columns which are partition columns.
-        :return: 
+        :return:
         """
         if table_name in self.keyspace_tables.keys():
             raise Exception(
@@ -126,9 +120,8 @@ class DaskCassandraLoader(object):
     def unload_cassandra_table(self, table_name):
         """
         Deletes the DaskDataframe and removes the table from the list of 'loaded' tables.
-        
-        :param table_name: It is a String. 
-        :return: 
+        :param table_name: It is a String.
+        :return:
         """
         del self.keyspace_tables[table_name].data
         del self.keyspace_tables[table_name]
