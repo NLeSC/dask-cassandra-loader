95a81422832f5dbec24ae9240e7b8f0b3a365468
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2019-10-01 17:05:40 +0200

    covadicy

--- tests/test_dask_cassandra_loader.py
+++ tests/test_dask_cassandra_loader.py
@@ -90,7 +90,8 @@ def test_table_load():
 
     # Load table 'tab1'
     dask_cassandra_loader.load_cassandra_table(
-        'tab1', ['id', 'year', 'month', 'day'], 
+        'tab1',
+        ['id', 'year', 'month', 'day'],
         [('month', 'less_than', 1), ('day', 'in_', [1, 2, 3, 8, 12, 30])],
         [(id, [1, 2, 3, 4, 5, 6]), ('year', [2019])],
         force=False
