16021726f269efc967556c0bbedc9b650d820e72
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2019-10-04 15:55:49 +0200

    test Dask

--- tests/test_dask_cassandra_loader.py
+++ tests/test_dask_cassandra_loader.py
@@ -11,7 +11,7 @@ from cassandra.protocol import NumpyProtocolHandler
 from cassandra.auth import PlainTextAuthProvider
 from dask_cassandra_loader import PagedResultHandler
 from dask_cassandra_loader.dask_cassandra_loader import DaskCassandraLoader
-
+from dask.distributed import Client
 
 def test_cassandra_connection():
     auth = PlainTextAuthProvider(username='cassandra', password='cassandra')
@@ -56,30 +56,24 @@ def test_cassandra_connection():
     return
 
 
-#def test_dask_connection():
-#    dask_cassandra_con = DaskCassandraLoader()
-#
-#    # Connect to a local Dask
-#    dask_cassandra_con.connect_to_local_dask()
-#
-#    def square(x):
-#        return x ** 2
-#
-#    def neg(x):
-#        return -x
-#
-#    # Run a computation on Dask
-#    a = dask_cassandra_con.dask_client.map(square, range(10))
-#    b = dask_cassandra_con.dask_client.map(neg, a)
-#    total = dask_cassandra_con.dask_client.submit(sum, b)
-#    result = dask_cassandra_con.dask_client.gather(total)
-#
-#    if result != -285:
-#        dask_cassandra_con.disconnect_from_dask()
-#        raise AssertionError()
-#
-#    dask_cassandra_con.disconnect_from_dask()
-#    return
+def test_dask_connection():
+    client = Client(processes=False)
+    def square(x):
+        return x ** 2
+
+    def neg(x):
+        return -x
+
+    # Run a computation on Dask
+    a = client.map(square, range(10))
+    b = client.map(neg, a)
+    total = client.submit(sum, b)
+    result = client.gather(total)
+
+    if result != -285:
+        raise AssertionError()
+
+    return True
 
 
 def test_table_load_empty():
