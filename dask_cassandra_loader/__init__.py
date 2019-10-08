9d3a2423ae2ae36d7a7664090999def8219c9d52
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2019-10-08 18:37:34 +0200

    Clean

--- dask_cassandra_loader/__init__.py
+++ dask_cassandra_loader/__init__.py
@@ -1,26 +1,8 @@
 # -*- coding: utf-8 -*-
+"""Documentation about the Dask Cassandra Loader module."""
 
-import logging
-
-from dask_cassandra_loader.__version__ import __version__
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+from .__version__ import __version__
+from .dask_cassandra_loader import DaskCassandraLoader, PagedResultHandler
 
 __author__ = "Romulo Goncalves"
 __email__ = 'r.goncalves@esciencecenter.nl'
-
-logger = logging.getLogger('cerulean')
-"""The Dask Cassandra Loader root logger. Use this to set Dask Cassandra Loader's log level.
-
-In particular, if something goes wrong and you want more debug output, you \
-can do::
-    import logging
-
-    dask_cassandra_loader.logger.setLevel(logging.INFO)
-
-or for even more::
-
-    dask_cassandra_loader.logger.setLevel(logging.DEBUG)
-"""
-
-__all__ = ['PagedResultHandler', 'CassandraOperators', 'CassandraConnector', 'CassandraLoadingQuery', 'CassandraLoadingQuery', 'CassandraTable']
