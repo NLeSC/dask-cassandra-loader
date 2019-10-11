16e47a66dcf61ab9511c7f384674c39683ac4309
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2019-10-11 16:43:21 +0200

    Revert, in the package name should say loader.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 # To update the package version number, edit dask-cassandra-loader/__version__.py
 version = {}
-with open(os.path.join(here, 'dask_cassandra', '__version__.py')) as f:
+with open(os.path.join(here, 'dask_cassandra_loader', '__version__.py')) as f:
     d = {}
     for line in f:
         (key, val) = line.split(' = ')
@@ -19,7 +19,7 @@ with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='dask_cassandra',
+    name='dask_cassandra_loader',
     version=version['__version__'],
     description="""A data loader which loads data from a Cassandra table into a Dask dataframe. It allows partition elimination,
         selection and projections pushdown.""",
@@ -28,12 +28,12 @@ setup(
     author_email='r.goncalves@esciencecenter.nl',
     url='https://github.com/NLeSC/dask-cassandra-loader',
     packages=[
-        'dask_cassandra',
+        'dask_cassandra_loader',
     ],
     include_package_data=True,
     license="Apache Software License 2.0",
     zip_safe=False,
-    keywords='dask_cassandra',
+    keywords='dask_cassandra_loader',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
