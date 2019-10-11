bdfd3eaf6bf0bf2069a4df7def5c38e8cc919200
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2019-10-11 16:24:02 +0200

    Rename

--- dask_cassandra_loader/__init__.py
+++ dask_cassandra_loader/__init__.py
@@ -2,7 +2,7 @@
 """Documentation about the Dask Cassandra Loader module."""
 
 from .__version__ import __version__
-from .dask_cassandra_loader import DaskCassandraLoader, PagedResultHandler
+from .loader import DaskCassandraLoader, PagedResultHandler
 
 __author__ = "Romulo Goncalves"
 __email__ = 'r.goncalves@esciencecenter.nl'
