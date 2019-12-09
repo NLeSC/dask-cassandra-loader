f1b25bf39cb10ec6a089f6cbacd990c9f04a47ff
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2019-12-09 17:17:44 +0100

    Fix test

--- tests/test_dask_cassandra_loader.py
+++ tests/test_dask_cassandra_loader.py
@@ -111,13 +111,12 @@ def test_table_load_empty():
     dask_cassandra_loader.connect_to_local_dask()
 
     # Load table 'tab1'
-    dask_cassandra_loader.load_cassandra_table(
+    table = dask_cassandra_loader.load_cassandra_table(
         'tab1', ['id', 'year', 'month', 'day'],
         [('month', 'less_than', [1]),
          ('day', 'in_', [1, 2, 3, 8, 12, 30])], [('id', [1, 2, 3, 4, 5, 6]),
                                                  ('year', [2019])],
         force=False)
-    table = dask_cassandra_loader.keyspace_tables['tab1']
 
     if table is None:
         raise AssertionError()
