c914464860f36481df80bf207fc3f3d856d3bf8e
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2019-12-09 17:48:31 +0100

    We have now a Dask Dataframe so we need to compute it.

--- tests/test_dask_cassandra_loader.py
+++ tests/test_dask_cassandra_loader.py
@@ -160,6 +160,8 @@ def test_table_load_with_data():
 
     if table.data is None:
         raise AssertionError("Table.data is not supposed to be None!!!")
+    else:
+        table.data.compute()
 
     # Inspect table information
     #print(table.data.head())
